9d0e38df053b83016afe3123a67e2b3e2e4cedd4
alexhateful <alexhateful@gmail.com>
2019-05-27 14:09:25 +0300

    add slick slider

---
+++ barbershop_v2/src/blocks/advantage/advantage.js
@@ -0,0 +1,35 @@
+/**
+ * @file Implementation of the advantage block
+ */
+
+// -------------------------- BEGIN MODULE VARIABLES --------------------------
+// TODO: add code here
+// --------------------------- END MODULE VARIABLES ---------------------------
+
+// -------------------------- BEGIN UTILITY FUNCTIONS -------------------------
+// TODO: add code here
+// --------------------------- END UTILITY FUNCTIONS --------------------------
+
+// ----------------------------- BEGIN DOM METHODS ----------------------------
+// TODO: add code here
+// ------------------------------ END DOM METHODS -----------------------------
+
+// --------------------------- BEGIN EVENT HANDLERS ---------------------------
+// TODO: add code here
+// ---------------------------- END EVENT HANDLERS ----------------------------
+
+// --------------------------- BEGIN PUBLIC METHODS ---------------------------
+/**
+ * Initialize the advantage block.
+ * @return true if the block is present on the page, false otherwise
+ */
+function initBlock() {
+    // TODO: add code here
+    $('.advantage__slider').slick();
+    return true;
+}
+// ---------------------------- END PUBLIC METHODS ----------------------------
+
+export default {
+    initBlock,
+};
